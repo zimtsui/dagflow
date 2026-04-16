75b8d2de3a0a1a3e842d180c6181d30079e24f98
Zim <zimtsui@gmail.com>
2026-04-16 17:32:43 +0800

    dev

--- examples/workflow.ts
+++ examples/workflow.ts
@@ -1,25 +1,17 @@
-import { Node } from '@zimtsui/dagflow';
+import { Node, Generator } from '@zimtsui/dagflow';
 declare function optimize(problem: string): Generator<string, string, string>;
-declare function evaluate1(problem: string): Generator<void, never, never>;
-declare function evaluate2(problem: string): Generator<boolean, never, never>;
+declare function evaluate1(problem: string, optimizer: Node<string, string, string>): Generator<void, never, never>;
+declare function evaluate2(problem: string, optimizer: Node<number, string, string>): Generator<boolean, never, never>;
 
 
 export async function workflow(problem: string): Promise<boolean> {
-    const optimizerDnm = {} satisfies Node.DepNodeMap.Proto;
-    const optimizerGen = optimize(problem);
-    await using optimizer = Node.create(optimizerGen, optimizerDnm);
-
-    const evaluate1Dnm: evaluate1.DepNodeMap = { optimizer };
-    const evaluate1Gen = evaluate1(problem, evaluate1Dnm);
-    await using evaluator1 = Node.create(evaluate1Gen, evaluate1Dnm);
-
-    const evaluate2Dnm: evaluate2.DepNodeMap = {
-        optimizer: optimizer.map(async s => Number.parseInt(s)),
+    await using optimizer = await Node.from(optimize(problem));
+    await using evaluator1 = await Node.from(evaluate1(problem, optimizer));
+    await using numberView = await optimizer.map(
+        async s => Number.parseInt(s),
         evaluator1,
-    };
-    const evaluate2Gen = evaluate2(problem, evaluate2Dnm);
-    await using evaluator2 = Node.create(evaluate2Gen, evaluate2Dnm);
-
-    const draft = await evaluator2.repeat();
-    return draft.extract();
+    );
+    await using evaluator2 = await Node.from(evaluate2(problem, numberView));
+    const debate = await evaluator2.next().then(r => r.value);
+    return debate.extract();
 };
--- src/node.ts
+++ src/node.ts
@@ -1,11 +1,12 @@
 import { Generator } from './generator.ts';
 import { Debate } from './debate.ts';
+import { Draft, Opposition } from './types.ts';
 
 
 
 export class Node<
     out draft, in rejection, out opposition,
-> implements AsyncIterator<Debate<draft, rejection, opposition>, never, void>, AsyncDisposable {
+> implements AsyncIterableIterator<Debate<draft, rejection, opposition>, never, void>, AsyncDisposable {
     /**
      * @param gencache Ownership transferred.
      */
@@ -27,5 +28,60 @@ export class Node<
     ): Promise<Node<draft, rejection, opposition>> {
         const gencache = await Generator.Cache.from(generator);
         return new Node(gencache);
+    }
+
+    public [Symbol.asyncIterator](): this {
+        return this;
+    }
+
+    public async map<nextdraft>(
+        f: (draft: draft) => PromiseLike<nextdraft>,
+        after: Node<unknown, never, unknown>,
+    ): Promise<Node<nextdraft, rejection, opposition>> {
+        return await Node.from(this.mapgen(f, after));
+    }
+
+    protected async *mapgen<nextdraft>(
+        f: (draft: draft) => PromiseLike<nextdraft>,
+        after: Node<unknown, never, unknown>,
+    ): Generator<nextdraft, rejection, opposition> {
+        for (;;) try {
+            const thisDebate = await this.next().then(r => r.value);
+            const afterDebate = await after.next().then(r => r.value);
+            const signal = AbortSignal.any([thisDebate.signal, afterDebate.signal]);
+            signal.throwIfAborted();
+            let output: Draft<nextdraft> | Opposition<opposition> = Draft.from(signal, await f(thisDebate.extract()));
+            for (;;) output = await thisDebate.next(yield output).then(r => r.value);
+        } catch (e) {
+            if (e instanceof Draft.AbortError) {} else throw e;
+        }
+    }
+
+    public static async all(nodes: Node<unknown, never, unknown>[]): Promise<Node<void, never, never>> {
+        return await Node.from(Node.allgen(nodes));
+    }
+
+    protected static async *allgen(nodes: Node<unknown, never, unknown>[]): Generator<void, never, never> {
+        for (;;) try {
+            const debates = await Promise.all(nodes.map(node => node.next().then(r => r.value)));
+            const signal = AbortSignal.any(debates.map(debate => debate.signal));
+            signal.throwIfAborted();
+            yield Draft.from(signal);
+            throw new Error();
+        } catch (e) {
+            if (e instanceof Draft.AbortError) {} else throw e;
+        }
+    }
+
+    public static async empty(): Promise<Node<void, never, never>> {
+        return await Node.from(Node.emptygen());
+    }
+
+    protected static async *emptygen(): Generator<void, never, never> {
+        for (;;) try {
+            yield Draft.from(new AbortSignal());
+        } catch (e) {
+            if (e instanceof Draft.AbortError) {} else throw e;
+        }
     }
 }
